fe90a76bab7cf8f95a54dbc3abc31fca8fec34a8
Inex Code <gitea@inex.rocks>
2023-08-30 10:40:39 +0300

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
     flake-utils.follows = "nix-vscode-extensions/flake-utils";
-    nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
   };
 
   outputs = { self, flake-utils, nixpkgs, nix-vscode-extensions }:
